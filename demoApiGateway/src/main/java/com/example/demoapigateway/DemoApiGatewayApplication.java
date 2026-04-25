62b17bdbac6d570b4285dbfaa220ca11d4126a0b
ounismoez98 <Moez.OUNIS@esprit.tn>
2026-04-25 22:34:53 +0100

    first commit

---
+++ demoApiGateway/src/main/java/com/example/demoapigateway/DemoApiGatewayApplication.java
@@ -0,0 +1,33 @@
+package com.example.demoapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorDsl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+
+public class DemoApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator gatewayroutes(RouteLocatorBuilder builder)
+	{
+		return  builder.routes()
+				.route("idroute1", r->r.path("/candidats/**")
+						.uri("lb://MSCandidat"))
+				.route("idroute2", r->r.path("/jobs/**")
+						.uri("lb://MS-job-s"))
+				.build();
+
+	}
+
+}
